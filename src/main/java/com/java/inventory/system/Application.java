33175359a1ae6359af81b32f107219a742dc8d14
ZIP-Norbs <norbert@zip.ph>
2025-10-14 16:25:33 +0800

    disables redis repository scanning

--- src/main/java/com/java/inventory/system/Application.java
+++ src/main/java/com/java/inventory/system/Application.java
@@ -3,9 +3,14 @@ package com.java.inventory.system;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
 @EnableCaching
+@EnableJpaRepositories(basePackages = "com.java.inventory.system.repository")
+// 👇 disables redis repository scanning since you only use it for caching
+@EnableRedisRepositories(basePackages = "none")
 public class Application {
 
 	public static void main(String[] args) {
