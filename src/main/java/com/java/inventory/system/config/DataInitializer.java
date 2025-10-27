8a8bdf54658630d345fb585db74b2c94f1ec5a05
ZIP-Norbs <norbert@zip.ph>
2025-10-28 00:20:47 +0800

    fix bug

--- src/main/java/com/java/inventory/system/config/DataInitializer.java
+++ src/main/java/com/java/inventory/system/config/DataInitializer.java
@@ -1,6 +1,5 @@
 package com.java.inventory.system.config;
 
-
 import com.java.inventory.system.model.User;
 import com.java.inventory.system.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -10,23 +9,25 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class DataInitializer {
-  @Bean
-  CommandLineRunner init(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
-    return args -> {
-      if (userRepository.findByUsername("user").isEmpty()) {
-        User u = new User();
-        u.setUsername("root");
-        u.setPassword(passwordEncoder.encode("root"));
-        u.setRoles("ROLE_USER");
-        u.setEmail("bobila.norbert@gmail.com");
-        u.setMobile("+639603717056");
-        userRepository.save(u);
-      }
-    };
-  }
 
-  @Bean
-  public BCryptPasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
+    @Bean
+    CommandLineRunner init(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+        return args -> {
+            // Check if root user already exists
+            if (userRepository.findByUsername("root").isEmpty()) {
+                User u = new User();
+                u.setUsername("root");
+                u.setPassword(passwordEncoder.encode("root"));
+                u.setRoles("ROLE_ADMIN");
+                u.setEmail("bobila.norbert@gmail.com");
+                u.setMobile("+639603717056");
+                userRepository.save(u);
+            }
+        };
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
